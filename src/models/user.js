4757f427d7f0deb14ddb134dba937d56097e54ca
Akash Goswami <akashgoswami70870@gmail.com>
2023-10-10 20:59:56 +0530

    Added the password hashing feature

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,10 @@
 const {
   Model
 } = require('sequelize');
+
+const bcrypt = require('bcrypt');
+
+const { SALT } = require('../config/serverConfig');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -31,6 +35,10 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'User',
+  });
+  User.beforeCreate((user) => {
+    const encryptedPassword = bcrypt.hashSync(user.password, SALT);
+    user.password = encryptedPassword;
   });
   return User;
 };
