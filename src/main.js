9632b22bcda01d5ab8148fe675d7cbee851b675d
Akash Goswami <akashgoswami70870@gmail.com>
2023-10-10 20:44:07 +0530

    Added some fixes for the bugs

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
+
 const { PORT } = require('./config/serverConfig');
 
+const apiRoutes = require('./routes/index');
 
 const app = express();
 
 const prepareAndStartServer = () => {
     app.listen(PORT,() => {
+
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({extended: true}));
+
+        app.use('/api', apiRoutes);
         console.log(`Server started listening on PORT: ${PORT}`);
     });
 }
