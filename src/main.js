40453dce08ae4740165ff210cc708346adfd7b46
Akash Goswami <akashgoswami70870@gmail.com>
2023-10-13 22:06:27 +0530

    Added user-roles

--- src/main.js
+++ src/main.js
@@ -1,22 +1,30 @@
 const express = require('express');
 
 const bodyParser = require('body-parser');
+const db = require('./models/index');   
 
 
 const { PORT } = require('./config/serverConfig');
 
 const apiRoutes = require('./routes/index');
 
+
 const app = express();
 
 const prepareAndStartServer = () => {
-    app.listen(PORT,() => {
+    app.listen(PORT, async() => {
 
         app.use(bodyParser.json());
         app.use(bodyParser.urlencoded({extended: true}));
 
         app.use('/api', apiRoutes);
         console.log(`Server started listening on PORT: ${PORT}`);
+
+        if (process.env.DB_SYNC){
+            db.sequelize.sync({alter : true});
+        }
+
+      
     });
 }
 
--- src/models/role.js
+++ src/models/role.js
@@ -11,6 +11,9 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      this.belongsToMany(models.User, {
+        through: 'User_Roles'
+      })
     }
   }
   Role.init({
